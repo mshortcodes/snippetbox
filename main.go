779fca3176612dbbfc0451dc9f9a88911ad790cf
mshortcodes <mshortcodes@gmail.com>
2025-08-27 11:52:20 -0700

    restrict subtree path

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
