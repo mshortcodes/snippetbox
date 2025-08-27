109df516a5c4cebaefcdb545c84208cd4c1abec3
mshortcodes <mshortcodes@gmail.com>
2025-08-27 11:40:25 -0700

    add paths

--- main.go
+++ main.go
@@ -8,11 +8,20 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Print("starting server on port 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	log.Fatal(http.ListenAndServe(":4000", mux))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("View a snippet here..."))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a snippet..."))
 }
