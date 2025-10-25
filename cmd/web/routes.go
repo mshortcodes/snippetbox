1734753ac6db7c41490e291fb5283bd345100537
mshortcodes <mshortcodes@gmail.com>
2025-10-24 19:20:12 -0700

    ch 10.7

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
